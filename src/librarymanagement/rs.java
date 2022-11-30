7a18ac80dda8bca6cbe1a280f27687e437cb06cd
Khushal Roop Rai <86585376+rai-khushal29@users.noreply.github.com>
2022-11-30 19:56:26 +0530

    Initial Build
    This is my academic project. This project contains some errors. They need MySQL for localhost and connector for this project.

---
+++ src/librarymanagement/rs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymanagement;
+
+/**
+ *
+ * @author khush
+ */
+class rs {
+
+    static boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getString(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
